b0fd6e669d7fe32a5381e9c2fb926957dcbce40b
Marcin Tabin <marcin.tabin@cognex.com>
2021-02-22 11:33:28 +0100

    Publishing tests.

--- JennyScienceControllerGUI/Properties/AssemblyInfo.cs
+++ JennyScienceControllerGUI/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Xenax controller")]
+[assembly: AssemblyTitle("JennyScience Xenax Controller GUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Xenax controller")]
+[assembly: AssemblyProduct("JennyScience Xenax Controller GUI")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
